33e7b9074fd85d8a82a887019a42e106d1cd94df
Omar Flórez <omarmfs98@protonmail.com>
2020-04-26 02:43:31 -0500

    build

--- src/components/PhoneDownload.vue
+++ src/components/PhoneDownload.vue
@@ -34,7 +34,7 @@ export default {
   computed: {
     ...mapGetters(["getLang"]),
     hour() {      
-      return moment.format('LT');
+      return moment().format('LT');
     },
     day() {
       moment.locale(this.getLang);
